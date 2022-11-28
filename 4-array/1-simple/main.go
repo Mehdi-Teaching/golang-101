c27af602413546269df0e3453e9768fc97ddfa96
Mehdi Teymorian <mehditeymorian322@gmail.com>
2022-11-28 13:49:07 +0330

    feat: update simple array section

--- 4-array/1-simple/main.go
+++ 4-array/1-simple/main.go
@@ -6,4 +6,7 @@ func main() {
 	numbers := []int{1, 2, 3, 4, 5}
 
 	fmt.Println(numbers)
+	fmt.Println("first element: ", numbers[0])
+	fmt.Println("second element: ", numbers[1])
+	fmt.Println("...")
 }
