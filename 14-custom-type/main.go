3535f507007352a6813dd48534b86a0749fe2f76
Mehdi Teymorian <mehditeymorian322@gmail.com>
2022-12-14 01:53:40 +0330

    feat: add custom type

---
+++ 14-custom-type/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+type Gender string
+
+const (
+	Male   Gender = "male"
+	Female Gender = "female"
+)
+
+type Status int
+
+const (
+	Bad Status = iota
+	Normal
+	Good
+	Best
+)
+
+func main() {
+
+	x := Male
+
+	fmt.Println(x)
+
+}
